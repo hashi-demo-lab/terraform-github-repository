4f452dccc33394cb1d3945ba1df98519a57e08e0
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-01 12:28:21 +1000

    remove provider config

--- providers.tf
+++ providers.tf
@@ -3,8 +3,9 @@
 # If you are using them for auth then specify it with a comment in the provider block
 # providers.tf shouldn't be present in module definitions. They should generally inherit the root module provider unless you need to do aliasing
 
-
+/* 
 provider "github" {
   # Configuration options
   owner = var.github_org
 }
+ */
